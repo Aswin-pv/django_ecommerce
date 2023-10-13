d986f6e45ea2a38f6c777c7509b84894bbaa4753
Aswin pv <pvaswin888@gmail.com>
2023-10-13 19:39:00 +0530

    usermodel name changed to account

--- ecommerce/accounts/models.py
+++ ecommerce/accounts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser,BaseUserManager
 
 
-class User(AbstractBaseUser):
+class Account(AbstractBaseUser):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     username = models.CharField(max_length=50,unique=True)
